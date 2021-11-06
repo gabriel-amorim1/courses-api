342f6d9015d9f6aaa5d7258ea35e4cd3d1619de3
Gabriel Amorim <therealamorim@gmail.com>
2021-11-06 15:49:04 -0300

    feat: :sparkles: change password and update profile

--- auth/views.py
+++ auth/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.models import User
-from auth.serializers import RegisterSerializer
+from auth.serializers import ChangePasswordSerializer, RegisterSerializer, UpdateUserSerializer
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from auth.serializers import MyTokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import IsAuthenticated
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
@@ -13,3 +14,15 @@ class RegisterView(generics.CreateAPIView):
 class MyObtainTokenPairView(TokenObtainPairView):
     permission_classes = (AllowAny,)
     serializer_class = MyTokenObtainPairSerializer
+
+class ChangePasswordView(generics.UpdateAPIView):
+
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ChangePasswordSerializer
+
+class UpdateProfileView(generics.UpdateAPIView):
+
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UpdateUserSerializer
